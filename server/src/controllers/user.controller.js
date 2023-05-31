2f28f7796a080dd1891b759593a0f3a9ac085a26
Kendall Madrigal <kendallmadrigal14@gmail.com>
2023-05-31 15:50:40 -0600

    changing users to controllers

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,16 +1,85 @@
-const express = require('express');
-const router = express.Router();
-const cors = require('cors');
-const multer = require('multer');
+const userSchema = require('../models/user.model');
 
-const userController = require('../controllers/userController');
+function validateUserData(req, res, next) {
+    const { name, email, password } = req.body;
 
-router.use(cors());
+    if (!name || !email || !password) {
+        return res.status(400).json({ message: 'Todos los campos son obligatorios.' });
+    }
 
-router.post('/user', userController.validateUserData, userController.createUser);
-router.get('/user', userController.getUsers);
-router.get('/user/:id', userController.getUserById);
-router.put('/user/:id', userController.validateUserData, userController.updateUser);
-router.delete('/user/:id', userController.deleteUser);
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+        return res.status(400).json({ message: 'El correo electrónico no tiene un formato válido.' });
+    }
 
-module.exports = router;
+    next();
+}
+
+function createUser(req, res) {
+    const user = new userSchema({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    });
+
+    user
+        .save()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+}
+
+function getUsers(req, res) {
+    userSchema
+        .find()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+}
+
+function getUserById(req, res) {
+    const { id } = req.params;
+    userSchema
+        .findById(id)
+        .then((data) => {
+            if (!data) {
+                return res.status(404).json({ message: 'Usuario no encontrado.' });
+            }
+            res.json(data);
+        })
+        .catch((error) => res.json({ message: error }));
+}
+
+function updateUser(req, res) {
+    const { id } = req.params;
+    const { name, email, password } = req.body;
+    userSchema
+        .findByIdAndUpdate(id, { $set: { name, email, password } }, { new: true })
+        .then((data) => {
+            if (!data) {
+                return res.status(404).json({ message: 'Usuario no encontrado.' });
+            }
+            res.json(data);
+        })
+        .catch((error) => res.json({ message: error }));
+}
+
+function deleteUser(req, res) {
+    const { id } = req.params;
+    userSchema
+        .findByIdAndDelete(id)
+        .then((data) => {
+            if (!data) {
+                return res.status(404).json({ message: 'Usuario no encontrado.' });
+            }
+            res.json(data);
+        })
+        .catch((error) => res.json({ message: error }));
+}
+
+module.exports = {
+    validateUserData,
+    createUser,
+    getUsers,
+    getUserById,
+    updateUser,
+    deleteUser,
+};
